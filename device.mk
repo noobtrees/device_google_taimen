4f0303b2470353f5f44e42d114fe50b9d54566e4
Abhisek Devkota <ciwrl@lineageos.org>
2018-01-18 21:58:09 +0100

    IMS Support
    Build what we can
    
    Change-Id: I0c46469ed7dadb0ac56995accb87faac76dceead

--- device.mk
+++ device.mk
@@ -32,6 +32,12 @@ DEVICE_PACKAGE_OVERLAYS += device/google/taimen/overlay
 PRODUCT_PROPERTY_OVERRIDES += \
     net.tethering.noprovisioning=true
 
+# IMS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    RcsService \
+    PresencePolling
+
 # Audio
 PRODUCT_COPY_FILES += \
     device/google/taimen/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
